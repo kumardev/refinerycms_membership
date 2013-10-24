e093bbdf9f3c86da935f2faecbae78423d73eba6
kumardev <kumardevp@gmail.com>
2013-10-24 14:16:24 +0530

    Update refinerycms_membership_email_parts.rb

--- db/seeds/refinerycms_membership_email_parts.rb
+++ db/seeds/refinerycms_membership_email_parts.rb
@@ -1,8 +1,8 @@
-Refinery::Memberships::MembershipEmailPart.new({
+Refinery::Memberships::MembershipEmailP.new({
   :title => 'email_header',
   :body => '<p>Header</p>'
 }).save(:validate => false)
-Refinery::Memberships::MembershipEmailPart.new({
+Refinery::Memberships::MembershipEmailP.new({
   :title => 'email_footer',
   :body => '<p>Foter</p>'
 }).save(:validate => false)
